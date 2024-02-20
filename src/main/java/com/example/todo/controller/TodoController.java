7557e19dd0180e7bd125810fc7485f5a3ab395dd
bonon <andrei.bonon@outlook.com>
2024-02-20 19:10:29 -0300

    delete ajustado

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -30,7 +30,7 @@ public class TodoController {
         return todoService.update(todo);
     }
 
-    @DeleteMapping
+    @DeleteMapping("{id}")
     List<Todo> delete(@PathVariable("id") Long id) {
         return todoService.delete(id);
     }
